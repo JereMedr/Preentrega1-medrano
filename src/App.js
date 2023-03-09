75ded0378a2d877d913edf53e2e21092f0157bb2
JereMedr <jeremedrano228@gmail.com>
2023-03-09 13:59:03 -0300

    renderizado item detail

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
+import ItemDetail from './components/ItemDetail';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
+    <NavBar />
 
-      <Routes>
-        <Route
-          path="/"
-          element={<ItemListContainer greeting={"Bienvenidos"} />}
-        />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route
+        path="/"
+        element={<ItemListContainer greeting={"Nuestros autos"} />}
+      />
+      <Route path="/detalle/:id" element={<ItemDetail />} />
+
+    </Routes>
+  </BrowserRouter>
+    // <BrowserRouter>
+    //   <NavBar />
+
+    //   <Routes>
+    //     <Route
+    //       path="/"
+    //       element={<ItemListContainer greeting={"Bienvenidos"} />}
+    //     />
+    //   </Routes>
+    // </BrowserRouter>
   );
 }
 
--- src/components/ItemDetail/index.js
+++ src/components/ItemDetail/index.js
@@ -2,24 +2,27 @@ import React, { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import data from "../../data.json";
 import { useParams } from "react-router-dom";
+import { useState } from "react";
 function ItemDetail() {
     const params = useParams();
     const idAuto = params.id;
     console.log(idAuto);
     console.log(data.autos);
+    const [auto, setAuto] = useState({});
     useEffect(() => {
         const promesaItem = new Promise((resolve, reject) => {
             setTimeout(() => {
-                let encontrado = data.autos.find((item) => item.id === Number(idUser));
+                let encontrado = data.autos.find((item) => item.id === Number(idAuto));
                 resolve(encontrado);
             }, 500);
         });
 
-        promesaItem.then((respuesta) => setUser(respuesta));
+        promesaItem.then((respuesta) => setAuto(respuesta));
     }, []);
     return (
         <div>
             <h1>ItemDetail</h1>
+            <h2>{auto.modelo}</h2>
         </div>
     );
 }
